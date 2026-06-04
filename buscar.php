e681c1af1e9016316903494792fb679f96c67840
Ma. Inés Solís Orantes <325089878@alumno.enp.unam.mx>
2026-06-04 14:33:08 -0600

    feat(config): Une con Base de Datos localhost

--- buscar.php
+++ buscar.php
@@ -1,5 +1,39 @@
 <?php
+    include './config.php';
+    //$con = connect();
 
+    $texto_buscado= "";
+    $lista_resultados = array();
+    if(isset($_GET["busqueda"])){
+        $texto_buscado = $_GET["busqueda"];
+
+        $sql = "SELECT * FROM sets WHERE name LIKE '%" . $texto_buscado . "%'";
+        $resultado_query = mysqli_query(connect(), $sql);
+        //mysqli_query es un objeto iterable de la consulta, entida que podemos iterar como un arreglo asociativo
+        //var_dump($resultado_query);
+        
+        $fila = mysqli_fetch_assoc($resultado_query);
+        var_dump($fila);
+        echo "<br>";
+        
+        if($resultado_query)
+        {
+            while($fila = mysqli_fetch_assoc($resultado_query))
+            {
+                $theme_id=$fila["theme_id"];
+                $sql2 = "SELECT name FROM themes WHERE theme_id = $theme_id";
+                $query2 = mysqli_query(connect(), $sql2);
+                if($query2)
+                {
+                    $res = mysqli_fetch_assoc($query2);
+                }
+                var_dump($res);
+            }
+
+        }
+        
+        //echo $fila["count(*)"];
+    }
 ?>
 
 <!DOCTYPE html>
