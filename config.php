d840d4c0ccbc848291c9bb0540a2db89f5dd923c
Ma. Inés Solís Orantes <325089878@alumno.enp.unam.mx>
2026-06-04 14:41:23 -0600

    feat(buscador): Aparece la lista de resultados de la busqueda

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
-<?php    
-    const DBHOST = "localhost";
+
+<?php 
+    /*const DBHOST = "localhost";
     const DBUSER = "root";
     const PASSWORD = "";
     const DB = "lego";
@@ -10,5 +11,23 @@
         //var_dump($conexion);
         return $conexion;
     }
-    //$con = connect();
+    //$con = connect();*/
+    
+    const DBHOST = "localhost";
+    const DBUSER = "root";
+    const PASSWORD = "";
+    const DB = "lego";
+
+    function connect()
+    {
+        $conexion = mysqli_connect(DBHOST, DBUSER, PASSWORD, DB);
+        /*
+        --PASO : var_dump de $conexion:
+        --PASO: regresar variable para un archivo externo
+        */
+        return $conexion;
+        
+    }
+    // --PASO : llamar a la funcion antes de pasarla a otro archivo
+    $conexion = connect();
 ?>
