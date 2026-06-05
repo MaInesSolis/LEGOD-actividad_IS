d61f6f129abe21bb970e33904a7473dd2aec8d65
Ma. Inés Solís Orantes <325089878@alumno.enp.unam.mx>
2026-06-05 12:37:47 -0600

    feat(crear): Añade un set nuevo

--- elimina.php
+++ elimina.php
@@ -1,5 +1,5 @@
 <?php
-    include '/config.php';
+    include 'config.php';
 
     $mensaje = "";
     $clase_mensaje = "";
@@ -36,10 +36,10 @@
     </div>
 
     <!-- PHP --> 
-    <div class="mensaje <?php  ?>">
+    <div class="mensaje <?php echo $clase_mensaje ?>">
         <h3>Resultado de la operación:</h3>
         <!-- PHP --> 
-        <p><?php  ?></p>
+        <p><?php echo $mensaje ?></p>
         <br>
         <a href="index.html" style="color: #000; font-weight:bold;">Volver a los resultados de búsqueda</a>
     </div>
